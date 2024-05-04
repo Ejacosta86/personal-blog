180051e0963004a87d70aa5c257d14404008b9a6
ejacosta86 <ejacosta86@gmail.com>
2024-05-04 12:20:44 -0400

    html css update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # personal-blog
+
+Scope of work:
+
+In this challenge, I create a two-page website where users will input and view blog posts by the user. It includes building a content form.
+During this code, I gained practical JavaScript experience, explore the Document Object Model's power, and set the stage for more advanced tasks.
+
+## Website
+
+click link to view [website](https://github.com/Ejacosta86/personal-blog).
+
+
+
+## Preview
+
+![example website]() //add image once website is done
+
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ assests/js/logic.js
@@ -0,0 +1,27 @@
+const themeSwitcher = document.querySelector('#darkMode')
+const container = document.querySelector('.container');
+console.log(container)
+let mode = 'dark'
+preventDefault();
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', function () {
+    // If mode is dark, apply light background
+    if (mode === 'dark') {
+      mode = 'light';
+      container.setAttribute('class', 'light');
+
+    }
+    // If mode is light, apply dark background
+    else {
+      mode = 'dark';
+      container.setAttribute('class', 'dark');
+
+    }
+  });
+  
+  
+  themeSwitcher.addEventListener('click', function() {
+    
+  }
+  )
